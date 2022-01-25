27b9bf2a440c016e8af6894c7386e5427abbee43
timicienio <timmy960072@gmail.com>
2022-01-25 11:43:57 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Routes } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="m-3 bg-slate-400">Nav</div>
+      <Routes></Routes>
+    </div>
+  );
+}
